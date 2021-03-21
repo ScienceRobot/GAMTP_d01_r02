5a4515dc802a801153263f900de97d8c02665a37
tedhuntington <tedhuntington@gmail.com>
2021-03-21 01:47:49 -0700

    adding source

---
+++ GAMTP_d01_r02/atmel_start_pins.h
@@ -0,0 +1,55 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+#ifndef ATMEL_START_PINS_H_INCLUDED
+#define ATMEL_START_PINS_H_INCLUDED
+
+#include <hal_gpio.h>
+
+// SAME53 has 14 pin functions
+
+#define GPIO_PIN_FUNCTION_A 0
+#define GPIO_PIN_FUNCTION_B 1
+#define GPIO_PIN_FUNCTION_C 2
+#define GPIO_PIN_FUNCTION_D 3
+#define GPIO_PIN_FUNCTION_E 4
+#define GPIO_PIN_FUNCTION_F 5
+#define GPIO_PIN_FUNCTION_G 6
+#define GPIO_PIN_FUNCTION_H 7
+#define GPIO_PIN_FUNCTION_I 8
+#define GPIO_PIN_FUNCTION_J 9
+#define GPIO_PIN_FUNCTION_K 10
+#define GPIO_PIN_FUNCTION_L 11
+#define GPIO_PIN_FUNCTION_M 12
+#define GPIO_PIN_FUNCTION_N 13
+
+#define PA02 GPIO(GPIO_PORTA, 2)
+#define PA03 GPIO(GPIO_PORTA, 3)
+#define PA12 GPIO(GPIO_PORTA, 12)
+#define PA13 GPIO(GPIO_PORTA, 13)
+#define PA14 GPIO(GPIO_PORTA, 14)
+#define PA15 GPIO(GPIO_PORTA, 15)
+#define PA17 GPIO(GPIO_PORTA, 17)
+#define PA18 GPIO(GPIO_PORTA, 18)
+#define PA19 GPIO(GPIO_PORTA, 19)
+#define PA22 GPIO(GPIO_PORTA, 22)
+#define PA23 GPIO(GPIO_PORTA, 23)
+#define PB04 GPIO(GPIO_PORTB, 4)
+#define PB05 GPIO(GPIO_PORTB, 5)
+#define PC00 GPIO(GPIO_PORTC, 0)
+#define PC01 GPIO(GPIO_PORTC, 1)
+#define PC02 GPIO(GPIO_PORTC, 2)
+#define PC03 GPIO(GPIO_PORTC, 3)
+#define PC11 GPIO(GPIO_PORTC, 11)
+#define PC12 GPIO(GPIO_PORTC, 12)
+#define PC16 GPIO(GPIO_PORTC, 16)
+#define PC17 GPIO(GPIO_PORTC, 17)
+#define PC20 GPIO(GPIO_PORTC, 20)
+#define PC27 GPIO(GPIO_PORTC, 27)
+#define PC28 GPIO(GPIO_PORTC, 28)
+
+#endif // ATMEL_START_PINS_H_INCLUDED
