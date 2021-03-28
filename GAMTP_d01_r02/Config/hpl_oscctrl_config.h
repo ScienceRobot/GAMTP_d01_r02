736eec7b441767d90135cac03367ae176671f8d4
ScienceRobot <info@sciencerobot.com>
2021-03-28 14:44:13 -0700

    fixed clock hanging problem

--- GAMTP_d01_r02/Config/hpl_oscctrl_config.h
+++ GAMTP_d01_r02/Config/hpl_oscctrl_config.h
@@ -45,7 +45,7 @@
 // <0xF=>1000000us
 // <id> xosc0_arch_startup
 #ifndef CONF_XOSC0_STARTUP
-#define CONF_XOSC0_STARTUP 0
+#define CONF_XOSC0_STARTUP 6
 #endif
 
 // <q> Clock Switch Back
@@ -158,7 +158,7 @@
 // <0xF=>1000000us
 // <id> xosc1_arch_startup
 #ifndef CONF_XOSC1_STARTUP
-#define CONF_XOSC1_STARTUP 0
+#define CONF_XOSC1_STARTUP 6
 #endif
 
 // <q> Clock Switch Back
@@ -404,7 +404,7 @@
 // <i> Select the clock source.
 // <id> fdpll0_ref_clock
 #ifndef CONF_FDPLL0_GCLK
-#define CONF_FDPLL0_GCLK GCLK_PCHCTRL_GEN_GCLK1_Val
+#define CONF_FDPLL0_GCLK GCLK_GENCTRL_SRC_XOSC1
 #endif
 
 // <h> Digital Phase Locked Loop Control
@@ -440,14 +440,14 @@
 // <i> Value of LDR is calculated using Fclk_dpll=Fckr*(LDR+1+LDRFRAC/32) formula as given in datasheet. This value is directly written in to DPLLRATIO register
 // <id> fdpll0_ldr
 #ifndef CONF_FDPLL0_LDR
-#define CONF_FDPLL0_LDR 0x3b
+#define CONF_FDPLL0_LDR 0x77
 #endif
 
 // <o> Clock Divider <0x0-0x7FF>
 // <i> This Clock divider is only for XOSC clock input to DPLL
 // <id> fdpll0_clock_div
 #ifndef CONF_FDPLL0_DIV
-#define CONF_FDPLL0_DIV 0x2
+#define CONF_FDPLL0_DIV 0x5
 #endif
 
 // <q> DCO Filter Enable
@@ -488,7 +488,7 @@
 // <0x3=>XOSC1 clock reference
 // <id> fdpll0_arch_refclk
 #ifndef CONF_FDPLL0_REFCLK
-#define CONF_FDPLL0_REFCLK 0x2
+#define CONF_FDPLL0_REFCLK 0x3
 #endif
 
 // <q> Wake Up Fast
